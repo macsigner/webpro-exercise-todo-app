a696df09719e54cc117f28bcbd4d18b2b4aee6b2
Mac Signer <macsigner@gmx.ch>
2022-03-04 17:21:48 +0100

    Add basic delete functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,3 +5,11 @@ const nlModeToggles = document.querySelectorAll('[data-mode-switch]');
 nlModeToggles.forEach(el => el.addEventListener('click', e => {
     document.documentElement.classList.toggle('dark-mode');
 }));
+
+const nlDeleteTodoItem = document.querySelectorAll('[data-todo-item-delete]');
+
+nlDeleteTodoItem.forEach(el => el.addEventListener('click', e => {
+    e.preventDefault();
+
+    e.target.closest('.todo-item').remove();
+}));
