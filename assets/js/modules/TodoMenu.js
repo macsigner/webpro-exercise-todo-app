3e562bbaba007602749d3373ba853fd3853faaa7
Mac Signer <macsigner@gmx.ch>
2022-04-27 19:04:28 +0200

    Move add functionality to menu class

--- assets/js/modules/TodoMenu.js
+++ assets/js/modules/TodoMenu.js
@@ -10,14 +10,17 @@ class TodoMenu extends BaseModule {
         this.itemTemplate = this._getTemplate();
         this.menu.addEventListener('itemsReady', this._buildMenu.bind(this));
 
+        this.form = this.el.querySelector('[data-todo-form]');
+        this.form.addEventListener('submit', this._todoFormListener.bind(this));
+
         this._readItems();
     }
 
-    add(value, id) {
-
+    add(obj) {
+        this._createItem(obj);
     }
 
-    remove(el) {
+    remove(task) {
 
     }
 
@@ -27,6 +30,25 @@ class TodoMenu extends BaseModule {
 
     filter(fn) {
 
+    }
+
+    _todoFormListener(e) {
+        e.preventDefault();
+
+        let todo = {
+            state: 'active',
+        }
+
+        let formData = new FormData(this.form);
+
+        for (let entry of formData.entries()) {
+            todo[entry[0]] = entry[1];
+            formData.delete(entry[0]);
+        }
+
+        this.add(todo);
+
+        this.form.reset();
     }
 
     _getTemplate() {
@@ -64,7 +86,7 @@ class TodoMenu extends BaseModule {
         this.menu.dispatchEvent(new CustomEvent('itemsReady', {
             detail: {
                 TodoMenu: this,
-            }
+            },
         }));
     }
 
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,31 +12,6 @@ const deleteItem = function(e) {
     e.target.closest('.todo-item').remove();
 }
 
-let itemId = 10;
-const addTodoItem = function(el) {
-    let todoList = document.querySelector('[data-todo-list]');
-
-    let todoItem = document.createElement('li');
-
-    todoItem.innerHTML = `<li class="todo-item">
-                          <input class="todo-item__checkbox" type="checkbox" id="todo-item-id-${itemId}">
-                          <label for="todo-item-id-${itemId}" class="todo-item__label">
-                          <span class="todo-item__pseudo-checkbox"></span>
-                          ${el.value}
-                          <span class="todo-item__actions"> <button class="todo-item__delete" data-todo-item-delete="">Delete</button></span>
-                          </label>
-                          </li>`
-
-    itemId++;
-
-    el.value = '';
-    todoItem.querySelector('[data-todo-item-delete]').addEventListener('click', deleteItem);
-    todoItem.querySelectorAll('[type="checkbox"]').forEach(el => {
-        el.checked = false
-    });
-    todoList.appendChild(todoItem);
-}
-
 const nlModeToggles = document.querySelectorAll('[data-mode-switch]');
 
 nlModeToggles.forEach(el => el.addEventListener('click', e => {
@@ -46,13 +21,3 @@ nlModeToggles.forEach(el => el.addEventListener('click', e => {
 const nlDeleteTodoItem = document.querySelectorAll('[data-todo-item-delete]');
 
 nlDeleteTodoItem.forEach(el => el.addEventListener('click', deleteItem));
-
-const elTodoItemAdd = document.querySelector('[data-todo-item-add]');
-
-if (elTodoItemAdd) {
-    elTodoItemAdd.addEventListener('keyup', e => {
-        if (e.key === 'Enter' && e.target.value.trim() !== '') {
-            addTodoItem(e.target);
-        }
-    })
-}
