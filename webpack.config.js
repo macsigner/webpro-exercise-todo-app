14b405ba66f438e843db71be92ab52a188657ee1
Mac Signer <macsigner@gmx.ch>
2022-07-01 19:23:47 +0200

    Rename main js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/js/main.js',
+    entry: './src/js/app.js',
     output: {
-        filename: 'main.js',
+        filename: 'app.js',
         path: path.resolve(__dirname, 'dist')
     }
 }
